e09bfc3cfd27cdd5e77d3509e8406298e0983d16
Lukas Hankeln <lukashankeln@googlemail.com>
2025-03-14 07:34:10 +0100

    feat: enable semantic commit style types for commits

--- generator.ts
+++ generator.ts
@@ -36,6 +36,20 @@ const renovateConfig = {
             matchStrings: [genericDockerImageMatch]
         }
     ],
+    packageRules: [
+        {
+            matchUpdateTypes: ["patch"],
+            semanticCommitType: "fix"
+        },
+        {
+            matchUpdateTypes: ["minor"],
+            semanticCommitType: "feat"
+        },
+        {
+            matchUpdateTypes: ["major"],
+            semanticCommitType: "feat!"
+        },
+    ],
 }
 
 const filename = `default.json`;
